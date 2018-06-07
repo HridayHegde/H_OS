40942d253f42aa2108e5551de0e8083957fb6e41
HridayHegde <hridayhegde1999@gmail.com>
2018-06-07 20:12:05 +0530

    Setup

---
+++ H_OS/H_OS/Kernel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace H_OS
+{
+    public class Kernel: Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+        
+        protected override void Run()
+        {
+            Console.Write(">>");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "Intro":Console.WriteLine("Welcome To H_OS\nMade By Hriday Hegde");
+                    break;
+
+            }
+        }
+    }
+}
